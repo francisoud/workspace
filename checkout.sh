4dadfec79ee6b7555d6e30d029ba8a00dc1f24fa
francisoud <pub.cog@gmail.com>
2016-06-01 16:03:40 +0200

    update script for work conf

--- checkout.sh
+++ checkout.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# todo replace with auto install
-sudo ansible-galaxy install cchurch.scm
-
-
-# -e ansible_ssh_user=francisoud => workaround bug in https://galaxy.ansible.com/cchurch/scm/
-# Ansible 2.0 has deprecated the “ssh” from ansible_ssh_user, ansible_ssh_host, and ansible_ssh_port to become ansible_user, ansible_host, and ansible_port. If you are using a version of Ansible prior to 2.0, you should continue using the older style variables (ansible_ssh_*). These shorter variables are ignored, without warning, in older versions of Ansible.
-
-ansible-playbook playbook.yml -e ansible_ssh_user=francisoud
+ansible-playbook playbook.yml
